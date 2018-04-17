72458a392a3c414605a6efc88bacb6e7c3bc831b
mjavier <michaeljaviermota@gmail.com>
2018-04-17 15:52:55 -0400

    working the progress bar

--- app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
@@ -2,6 +2,12 @@ package com.londonappbrewery.quizzler;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -9,31 +15,84 @@ public class MainActivity extends Activity {
 
 
     // TODO: Declare member variables here:
-
+    Button mTrueButton;
+    Button mFalseButton;
+    TextView mScoreTextView;
+    TextView mQuestionTextView;
+    ProgressBar mProgressBar;
+    int mIndex;
+    int mScore;
+    int mQuestion;
 
     // TODO: Uncomment to create question bank
-//    private TrueFalse[] mQuestionBank = new TrueFalse[] {
-//            new TrueFalse(R.string.question_1, true),
-//            new TrueFalse(R.string.question_2, true),
-//            new TrueFalse(R.string.question_3, true),
-//            new TrueFalse(R.string.question_4, true),
-//            new TrueFalse(R.string.question_5, true),
-//            new TrueFalse(R.string.question_6, false),
-//            new TrueFalse(R.string.question_7, true),
-//            new TrueFalse(R.string.question_8, false),
-//            new TrueFalse(R.string.question_9, true),
-//            new TrueFalse(R.string.question_10, true),
-//            new TrueFalse(R.string.question_11, false),
-//            new TrueFalse(R.string.question_12, false),
-//            new TrueFalse(R.string.question_13,true)
-//    };
+    private TrueFalse[] mQuestionBank = new TrueFalse[] {
+            new TrueFalse(R.string.question_1, true),
+            new TrueFalse(R.string.question_2, true),
+            new TrueFalse(R.string.question_3, true),
+            new TrueFalse(R.string.question_4, true),
+            new TrueFalse(R.string.question_5, true),
+            new TrueFalse(R.string.question_6, false),
+            new TrueFalse(R.string.question_7, true),
+            new TrueFalse(R.string.question_8, false),
+            new TrueFalse(R.string.question_9, true),
+            new TrueFalse(R.string.question_10, true),
+            new TrueFalse(R.string.question_11, false),
+            new TrueFalse(R.string.question_12, false),
+            new TrueFalse(R.string.question_13,true)
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mTrueButton = (Button)findViewById(R.id.true_button);
+        mFalseButton = findViewById(R.id.false_button);
+        mQuestionTextView = findViewById(R.id.question_text_view);
+        mScoreTextView   = findViewById(R.id.score);
+        mProgressBar = findViewById(R.id.progress_bar);
+       mQuestion= mQuestionBank[mIndex].getQuestionID();
+         mQuestionTextView.setText(mQuestion);
 
+
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Log.d("Quizzler","Button pressed!");
+//                Toast.makeText(getApplicationContext(),"true pressed!", Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
+                updateQuestion();
+            }
+        });
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast myToast = Toast.makeText(getApplicationContext(),"False pressed!", Toast.LENGTH_SHORT);
+//                        myToast.show();
+                checkAnswer(false);
+                updateQuestion();
+            }
+        });
 
     }
+
+    private void updateQuestion(){
+        mIndex = (mIndex + 1) % mQuestionBank.length;
+        mQuestion = mQuestionBank[mIndex].getQuestionID();
+        mQuestionTextView.setText(mQuestion);
+    }
+
+    private void  checkAnswer(boolean userSelection){
+        boolean correctAnswer = mQuestionBank[mQuestion].isAnswer();
+        if ( correctAnswer == userSelection ){
+            Toast.makeText(getApplicationContext(),R.string.correct_toast,Toast.LENGTH_SHORT).show();
+
+        }else{
+
+            Toast.makeText(getApplicationContext(),R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
 }
---
+++ app/src/main/java/com/londonappbrewery/quizzler/TrueFalse.java
@@ -0,0 +1,29 @@
+package com.londonappbrewery.quizzler;
+
+public class TrueFalse {
+
+    private int mQuestionID;
+    private boolean mAnswer;
+
+    public TrueFalse(int questionsResourceID, boolean trueOrFalse){
+        mQuestionID = questionsResourceID;
+        mAnswer = trueOrFalse;
+    }
+
+    public int getQuestionID() {
+        return mQuestionID;
+    }
+
+    public void setQuestionID(int questionID) {
+        mQuestionID = questionID;
+    }
+
+    public boolean isAnswer() {
+        return mAnswer;
+    }
+
+    public void setAnswer(boolean answer) {
+        mAnswer = answer;
+    }
+
+}
